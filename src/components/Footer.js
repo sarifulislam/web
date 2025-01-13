6778c3ef5b195d3e93b3d1d57e141255a5f7fe39
Sariful Islam <sarifulislam680@gmail.com>
2025-01-13 09:23:19 +0530

    changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -16,7 +16,7 @@ const Footer = () => {
                         <div className="box-border border-b-4 border-blue-900 p-8 bg-gray-200 text-gray-600 text-center rounded-lg xl:w-80 mx-auto">
                             <h3 className="font-bold text-4xl mb-4">SSI Tech Solution</h3>
                             <div className='text-md font-medium text-gray-600'>
-                                <h5>Kolghat, Purba Medinipur</h5>
+                                <h5>Kolaghat, Purba Medinipur</h5>
                                 <p>West Bengal</p>
                                 <p>721151</p>
                                 <p>India</p>
