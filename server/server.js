757dc280a448bce97268f2ca75a203372f5c1f6a
Sariful Islam <sarifulislam680@gmail.com>
2025-02-09 15:07:03 +0530

    changes\

--- server/server.js
+++ server/server.js
@@ -3,13 +3,31 @@ const xlsx = require('xlsx');
 const { Storage } = require('@google-cloud/storage');
 const multer = require('multer');
 const cors = require('cors'); // Import the CORS package
+const { initializeApp } = require('firebase/app'); // Import Firebase
+const { getFirestore } = require('firebase/firestore'); // Import Firestore
+const { getAuth } = require('firebase/auth'); // Import Firebase Authentication
+
 const dotenv = require('dotenv');
 
 dotenv.config(); // Load environment variables
 
 const app = express();
 const PORT = process.env.PORT || 5000; // Use PORT from environment or default to 5000
-const bucketName = "backed_data"; 
+const firebaseConfig = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+const db = getFirestore(firebaseApp);
+const auth = getAuth(firebaseApp);
+const bucketName = process.env.BUCKET_NAME; 
+
 
 // Check if required environment variables are set
 if (!bucketName) {
